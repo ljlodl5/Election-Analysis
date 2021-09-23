8b5dd692b030d27d34ff9e2bc1f7d7884a09eef8
LJ <ljlodl5@att.net>
2021-09-23 10:47:07 -0500

    Completed winning code

--- pyPoll.py
+++ pyPoll.py
@@ -11,6 +11,10 @@ total_votes = 0
 #declare new list for candidate 
 candidate_options=[]
 candidate_votes={}
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 # Open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -39,6 +43,25 @@ for candidate_name in candidate_votes:
     votes = candidate_votes[candidate_name]
     # 3. Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
-    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+   # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidate_name
+
+#  To do: print out the winning candidate, vote count and percentage to
+#   terminal.
+
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 # 3. Print the total votes.
-print(candidate_votes)
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
